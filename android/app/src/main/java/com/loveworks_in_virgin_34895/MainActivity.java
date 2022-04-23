e4f410d35da62559e05fa3840a9d8bb210e80b3d
Crowdbotics <team@crowdbotics.com>
2022-04-23 21:01:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/loveworks_in_virgin_34895/MainActivity.java
@@ -0,0 +1,15 @@
+package com.loveworks_in_virgin_34895;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "loveworks_in_virgin_34895";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "loveworks_in_virgin_34895",
+  url: "https://loveworks_in_virgin_34895.botics.co",
+  api: "https://loveworks_in_virgin_34895.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
